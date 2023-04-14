a0895a0fa93820f659762d12693c09a1eb3b2663
jkokilaCSE <jkokila2810@gmail.com>
2023-04-14 17:17:48 +0530

    chrome extension using js started

---
+++ chrome_Extension.js
@@ -0,0 +1,10 @@
+let myLeads=[]
+const inputEl=document.getElementById("input-el")
+
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
